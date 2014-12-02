593c2c50acb913b97ceb16c3a82023f07e526b65
Tyler George <tyler.george@live.com>
2014-12-02 01:25:09 -0600

    formatting

--- waiting.php
+++ waiting.php
@@ -1,60 +1,32 @@
 <html>
-
-
-<head>
+    <head>
 	<title>Waiting</title>
-	<LINK REL="stylesheet" TYPE="text/css" HREF="css/style.css">
+	<link REL="stylesheet" TYPE="text/css" HREF="css/style.css">
 	<script src="js/jquery.js"></script>
         <script src="js/jquery.cookie.js"></script>
 	<script src="api/api.js"></script>
 	<script src="js/style.js"></script>
-</head>
-
-
-<body>
-
-
-	<div class="wrapper">
-
-		<header>
-
-			<div class="userPass">
-
-		
-                        <?php
+    </head>
+    <body>
+        <div class="wrapper">
+            <header>
+                <div class="userPass">
+                    <?php
 			if(!isset($_COOKIE['user'])){
 				header("location: index.php");
-			}else{
-				echo "Welcome " . $_COOKIE['user'];
-				echo '<input type="submit" value="Logout" onclick="logoutBtn(\'' . $_COOKIE['user'] . '\')">'; 
+			} else {
+                            echo "Welcome " . $_COOKIE['user'];
+                            echo '<input type="submit" value="Logout" onclick="logoutBtn(\'' . $_COOKIE['user'] . '\')">'; 
 			}
-			?>
-
-			</div>
-			
-		</header>
-
-
-		<div class="mainBody2">
-
-			
-
-<div class="WaitingHeading">
-
-<h1>  Waiting for another player to join ya ...</h1>
-
-<img src="img/wait.jpg" />
-</div>
-
-
-
-
-		</div>
-
-	</div>
-
-</body>
-
-
-
+                    ?>
+                </div>
+            </header>
+            <div class="mainBody2">
+                <div class="WaitingHeading">
+                    <h1>  Waiting for another player to join ya ...</h1>
+                    <img src="img/wait.jpg" />
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
