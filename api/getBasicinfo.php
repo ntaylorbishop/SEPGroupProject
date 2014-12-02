eaddc378d34949f6c0fb6bd784e624f9f26528dc
ntaylorbishop <ntaylorbishop@gmail.com>
2014-12-02 17:28:14 -0600

    getBasicInfo

--- api/getBasicinfo.php
+++ api/getBasicinfo.php
@@ -1,11 +1,8 @@
 <?php
 require 'db_conn.php';
 $username = $_GET['username'];
-$json = file_get_contents("php://input");
-$user = json_decode($json);
-$username = $user->username;
 
-$sql = "SELECT user1, user2 FROM Whos_Playing WHERE username = :username";
+$sql = "SELECT user1, user2,  user1Pieces, user2Pieces, user1Time, user2Time FROM Whos_Playing WHERE user1 = :username OR user2 = :username";
 try {
     $db = dbconnect();
     $stmt = $db->prepare($sql);
@@ -14,7 +11,7 @@ try {
     if ($stmt->rowCount() != 1) {
         echo '{"error": true}';
     } else {
-        echo $stmt->fetch(PDO::FETCH_ASSOC);
+        echo json_encode($stmt->fetch(PDO::FETCH_ASSOC));
     }
 
     $db = null;
