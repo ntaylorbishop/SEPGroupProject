865b6fce7421b50a2bf550b257d976c92f926ea0
ntaylorbishop <ntaylorbishop@gmail.com>
2014-12-02 01:03:21 -0600

    Added connecting players

--- js/style.js
+++ js/style.js
@@ -8,12 +8,19 @@ function logoutBtn(username) {
 	location.reload();
 }
 
+function connectPlayers(user1, user2) {
+	//Call to API connect players here
+}
+
 $(document).ready(function() {
 	var users = JSON.parse(getUsersWaiting());
 
 	for(var i = 0; i < users.length; i++) {
 		$('.lobbiesList').append("<p>" + users[i].username + "</p>");
-
+		$('div.lobbiesList p').click(function() {
+   			window.location.href='game.html';
+   			connectPlayers($('.lobbiesList p').text(), $.cookie('user'));
+		});
 	}
 
 });
